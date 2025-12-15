358efbccf0b2cf631236e7fac7c6ed5f5cfa2545
kaelo-ai <kaelomotaung.ai@gmail.com>
2025-12-15 11:47:24 +0200

    TSLA

---
+++ 0-Functions-Variables/tesla.py
@@ -0,0 +1,11 @@
+# Variables
+## A variable is used to store values (objects).
+# Variable Declartion / Definition
+# variable name = "value"
+companyName = "Tesla"  # string
+primaryExchange = "NASDAQ"  # string  primaryExchange
+ceo = "Elon Musk"
+marketCap = 1.44  # float  / double
+peRatio = 303.99  # float
+ticker = "TSLA"  # float
+currentPrice = 458.96

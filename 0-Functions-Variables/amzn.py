71eaab2373c27b14fe759b728d6c06cc3c5285ff
Lihle Diphokwana <lihlegabrieldiphokwana@gmail.com>
2025-12-15 13:53:52 +0200

    amzn info

--- 0-Functions-Variables/amzn.py
+++ 0-Functions-Variables/amzn.py
@@ -5,15 +5,21 @@ print("Add add a new company to listings...")
 # input function / captures an input and assignes it to a variable
 companyName = input("Company Name: ")
 ceo = input("CEO: ")
+marketCap = input("Market Cap: ")
+peRatio = input("PE Ratio: ")
+#create function 
 
 
-def companyInfo(name, ceo, exchange="NASDAQ"):
+def companyInfo(name, ceo, marketCap, exchange="NASDAQ"):
     print("_________________________________________")
     print("Company Name: ", name)
     print("Exchange:", exchange)
     print("Ceo: ", ceo)
+    print("Market Cap:", marketCap)
+    print("PE Ratio: ", peRatio)
     print("_________________________________________")
 
 
 # Function Call
-companyInfo(companyName, ceo)
+companyInfo(companyName, ceo, marketCap,)
+print ("Success!")

a32b5baea5321ed41bb51dccbcb4c9179b789485
Lihle Diphokwana <lihlegabrieldiphokwana@gmail.com>
2025-12-15 12:29:00 +0200

    MSFT comapny info

---
+++ 0-Functions-Variables/MSFT.py
@@ -0,0 +1,19 @@
+#step 1 declare variables
+welcomeMessage = "Welcome to Thirdspace list here is our listings"
+companyName = "Microsoft"
+primaryExchange = "NASDAQ"
+ceo = "Satya Nadella"
+marketCap = 3.56
+peRatio = 34.05
+ticker = "MSFT"
+currentPrice =478.53
+#step 2 print variables
+print (welcomeMessage)
+print ("1.")
+print("\tComapny Name:", companyName)
+print("\tPrimary Exchange:", primaryExchange)
+print("\tCeo:", ceo)
+print("\tmarketCap:" , marketCap)
+print("\tPE Ratio:", peRatio)
+print("\tTicker:", ticker)
+print("\tCurrent Price:",currentPrice)

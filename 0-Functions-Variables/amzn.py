bd48e4c70bfc131323970f343923b4c1985a516e
Lihle Diphokwana <lihlegabrieldiphokwana@gmail.com>
2025-12-17 22:11:26 +0200

    loop creation, still needs fix

--- 0-Functions-Variables/amzn.py
+++ 0-Functions-Variables/amzn.py
@@ -3,7 +3,7 @@ from email.mime import base
 
 welcomeMessage = "Welcome to KLS Trading!!!!!"
 print(welcomeMessage)
-print("Add add a new company to listings... - amzn.py:3")
+print("Add add a new company to listings...")
 
 # input function / captures an input and assignes it to a variable
 companyName = input("Company Name: ")
@@ -23,9 +23,9 @@ selection = [(0,"NASDAQ"), (1,"DOW"), (2,"S&P500"), (3,"FTSE"), (4,"DAX"), (5,"N
 
 
 def companyInfo(name, ceo, exchanges, marketCap,selection,):
-    print("________________________________________ - amzn.py:16")
-    print("Company Name: - amzn.py:17", name)
-    print("Ceo: - amzn.py:19", ceo)
+    print("________________________________________")
+    print("Company Name:", name)
+    print("Ceo:", ceo)
     print("Select exhange", list(enumerate(exchanges)))
     selection = str(input("Select exchange"))
     if selection == "NASDAQ":
@@ -38,16 +38,16 @@ def companyInfo(name, ceo, exchanges, marketCap,selection,):
     #print("PE Ratio: ", peRatio)
     pe = int(input("PE Ratio: "))
     if pe <50:
-            print("strong,buy - amzn.py:25")
+            print("strong,buy")
     if pe >50:
-            print("strong sell - amzn.py:27")
+            print("strong sell")
     elif pe ==50:
-            print("neutral - amzn.py:29")
-    print("_________________________________________ - amzn.py:30")
+            print("neutral")
+    print("_________________________________________")
 
 def tradeActivity(opening,closing, priceMovement):
-    print("Opening Price: - amzn.py:33", openingPrice)
-    print("Closing Price: - amzn.py:34", closingPrice)
+    print("Opening Price:", openingPrice)
+    print("Closing Price:", closingPrice)
     priceMovement = (float(closingPrice)- float(openingPrice))
     if priceMovement >0:
         print("Price went down by", priceMovement)
@@ -55,9 +55,18 @@ def tradeActivity(opening,closing, priceMovement):
         print("Price went up by", priceMovement)
     elif priceMovement ==0:
         print("No price movement")
+
+def startAgain(choiceYes, choiceNo, input, navigate):
+    print("Would you like to start again?", choiceYes, "or", choiceNo)
+    if input == (bool(choiceYes)): 
+           navigate = companyInfo(companyName, ceo, exchanges, marketCap, selection=list,)
+    elif input == choiceNo:
+            print("Thank you for testing KLS Trading")
 # Function Call Company Info
 companyInfo(companyName, ceo, exchanges, marketCap, selection=list,)
 # Function Call Trade Activity
 tradeActivity(openingPrice, closingPrice, priceMovement,)
+#Function Call Start Again
+startAgain(choiceYes, choiceNo, bool, navigate=companyInfo(companyName, ceo, exchanges, marketCap, selection=list,))
 
-print ("Success! - amzn.py:38")
+print ("Success!")

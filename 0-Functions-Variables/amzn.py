011416391bb85448d5786edab39fe6ccac8f7a92
Lihle Diphokwana <lihlegabrieldiphokwana@gmail.com>
2025-12-17 21:25:21 +0200

    Added exchange selection + price movement

--- 0-Functions-Variables/amzn.py
+++ 0-Functions-Variables/amzn.py
@@ -1,4 +1,7 @@
-welcomeMessage = "Welcome to Thirdspace!!!!!"
+from email.mime import base
+
+
+welcomeMessage = "Welcome to KLS Trading!!!!!"
 print(welcomeMessage)
 print("Add add a new company to listings... - amzn.py:3")
 
@@ -6,18 +9,32 @@ print("Add add a new company to listings... - amzn.py:3")
 companyName = input("Company Name: ")
 ceo = input("CEO: ")
 marketCap = input("Market Cap: ")
+estYear = input("Year Founded? ")
+exchanges = ["NASDAQ","DOW","S&P500","FTSE","DAX","NIKKEI225"]
 openingPrice = input ("Opening Price: ")
 closingPrice = input ("Closing Price: ")
+choiceYes = bool(True)
+choiceNo = bool(False)
 priceMovement = (float(closingPrice)- float(openingPrice))
 peRatio = int(input("PE Ratio: "))
+list (enumerate(exchanges))
+selection = [(0,"NASDAQ"), (1,"DOW"), (2,"S&P500"), (3,"FTSE"), (4,"DAX"), (5,"NIKKEI225")]
 #create function 
 
 
-def companyInfo(name, ceo, marketCap, exchange="NASDAQ"):
+def companyInfo(name, ceo, exchanges, marketCap,selection,):
     print("________________________________________ - amzn.py:16")
     print("Company Name: - amzn.py:17", name)
-    print("Exchange: - amzn.py:18", exchange)
     print("Ceo: - amzn.py:19", ceo)
+    print("Select exhange", list(enumerate(exchanges)))
+    selection = str(input("Select exchange"))
+    if selection == "NASDAQ":
+          print("Session opens @ 16:30")
+    elif selection =="DOW":
+          print("Session opens @ 16:30")
+    elif selection == "S&P500" or selection == "FTSE" or selection == "DAX" or selection == "NIKKEI225":
+          print("Sorry you are looking at the wrong stock, try again later")
+
     #print("PE Ratio: ", peRatio)
     pe = int(input("PE Ratio: "))
     if pe <50:
@@ -39,7 +56,7 @@ def tradeActivity(opening,closing, priceMovement):
     elif priceMovement ==0:
         print("No price movement")
 # Function Call Company Info
-companyInfo(companyName, ceo, marketCap,)
+companyInfo(companyName, ceo, exchanges, marketCap, selection=list,)
 # Function Call Trade Activity
 tradeActivity(openingPrice, closingPrice, priceMovement,)
 
